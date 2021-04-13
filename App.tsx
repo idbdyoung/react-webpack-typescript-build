cf121a3307a943085cef1a912bb655158742c017
idbd <idbd.young@gmail.com>
2021-04-13 15:43:44 +0900

    feat: Add react-router and react-helmet

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import News from './pages/News';
 
 const App: React.FC = () => {
   return (
-    <div>Hello!</div>
+    <div>
+      <Helmet>
+        <title>App</title>
+      </Helmet>
+      <Route path='/' render={() => <Header />}/>
+      <Switch>
+        <Route exact path='/' render={() => <Home />}/>
+        <Route exact path='/news' render={() => <News />}/>
+      </Switch>
+      <Footer />
+    </div>
   );
 };
 
---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname),
+  entry: {
+    index: './index.tsx'
+  },
+  output: {
+    filename: '[name].js'
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+          'ts-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({}),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
